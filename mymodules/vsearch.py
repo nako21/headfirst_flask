1d4f940b14acbf1d4aef099cd1cfc0baaa6776cd
Анастасия <Anastasia@MacBook-Air-Anastasia-5.local>
2020-06-22 14:36:22 +0300

    python flask app

---
+++ mymodules/vsearch.py
@@ -0,0 +1,8 @@
+def search4vowels(phrase:str)->set:
+    """Возвращает гласные, найденные в указанном слове."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str,letters:str='aeiou')->set:
+    """Возвращает множество букв из 'letters', найденных в указанной фразе."""
+    return set(letters).intersection(set(phrase))
